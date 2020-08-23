787a64cc2f1b59239cf30836be9eb9ea738a69e5
Peter John Alvarado <peterjohn@gorated.ph>
2020-08-23 21:25:14 +0800

    [feat] Initialize http handler Initialize and Setup http handler for rest

--- main.go
+++ main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "products-api", log.LstdFlags)
+	// Initialize new servemux and bind handlers
+	sm := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:              ":9090",
+		Handler:           sm,
+		IdleTimeout:       120 * time.Second,
+		ReadHeaderTimeout: 1 * time.Second,
+		WriteTimeout:      1 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	// Initialize Channel
+	sigChan := make(chan os.Signal)
+	// Add os listener to kill, interrupt command
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	// Add Channel listener
+	sig := <-sigChan
+
+	logger.Println("Received Terminate, gracefully shutting down", sig)
+
+	// Graceful shutdown
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(tc)
+}
