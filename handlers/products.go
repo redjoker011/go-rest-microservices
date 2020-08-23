9f4da4dba9f76369178337242be2ef938f6ebc00
Peter John Alvarado <peterjohn@gorated.ph>
2020-08-23 22:38:21 +0800

    [feat] Response based handler Invoke action based on http method

--- handlers/products.go
+++ handlers/products.go
@@ -14,7 +14,18 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// Respond based on HTTP Method
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	// Raise error for unsupported HTTP Method
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	// Get products
 	lp := data.GetProducts()
 	// Convert struct into JSON and write into ResponseWriter
