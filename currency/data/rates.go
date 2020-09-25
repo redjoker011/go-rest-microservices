532014f9256cd6a1adc07ae07ca2045f400bc611
Peter John Alvarado <peterjohn@gorated.ph>
2020-09-25 22:44:06 +0800

    [feat] Add data package for currency

---
+++ currency/data/rates.go
@@ -0,0 +1,66 @@
+package data
+
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/hashicorp/go-hclog"
+)
+
+type ExchangeRates struct {
+	log   hclog.Logger
+	rates map[string]float64
+}
+
+func NewRates(l hclog.Logger) (*ExchangeRates, error) {
+	er := &ExchangeRates{log: l, rates: map[string]float64{}}
+
+	err := er.GetRates()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return er, nil
+}
+
+func (er *ExchangeRates) GetRates() error {
+	var url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml?"
+	resp, err := http.DefaultClient.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Expected error code 200 got %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+	md := &Cubes{}
+
+	xml.NewDecoder(resp.Body).Decode(&md)
+
+	for _, c := range md.CubeData {
+		r, err := strconv.ParseFloat(c.Rate, 64)
+		if err != nil {
+			return err
+		}
+
+		er.rates[c.Currency] = r
+	}
+
+	return nil
+}
+
+type Cube struct {
+	Currency string `xml:"currency,attr"`
+	Rate     string `xml:"rate,attr"`
+}
+
+type Cubes struct {
+	CubeData []Cube `xml:"Cube>Cube>Cube"`
+}
