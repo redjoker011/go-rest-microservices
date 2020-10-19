1a37e89692f9937a58905271c58441e08182d60c
Peter John Alvarado <peterjohn@gorated.ph>
2020-10-19 20:24:44 +0800

    [feat] Create SubscribeRates Streaming Service

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"io"
+	"time"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/redjoker011/online-cafe/currency/data"
@@ -29,4 +31,33 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 		return &protos.RateResponse{Rate: 0.0}, err
 	}
 	return &protos.RateResponse{Rate: rate}, nil
+}
+
+func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
+	// Create a block for streaming
+	go func() {
+		for {
+			rr, err := src.Recv()
+			if err == io.EOF {
+				c.log.Info("Client has closed the connection")
+				break
+			}
+
+			if err != nil {
+				c.log.Error("Unable to read from client", "error", err)
+				break
+			}
+
+			c.log.Info("Handle client request", "request", rr)
+		}
+	}()
+
+	for {
+		err := src.Send(&protos.RateResponse{Rate: 12.1})
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
